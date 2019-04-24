7ac5a3b89193b38885e91b3e7b71c3df9b4b67f6
iamconnaught <wdonnelly22@gmail.com>
2019-04-24 13:05:09 -0500

    created user model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Experience = require('./experiences')
+
+const userSchema = mongoose.Schema({
+	username: String,
+	password: String,
+	experience: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Experience'
+	}]
+})
+
+const User = mongoose.model('User', userSchema);
+
+
+
+module.exports = Article;
