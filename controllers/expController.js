700c762bea44173f1645e2b2b1cd885524b349ba
iamconnaught <wdonnelly22@gmail.com>
2019-04-24 12:43:18 -0500

    started expController

---
+++ controllers/expController.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const Experience = require('../models/experiences')
+const User = require('../models/users')
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
