64a5d659cd96fa65aa1090e5fe83431df461e3b5
iamconnaught <wdonnelly22@gmail.com>
2019-04-25 11:33:10 -0500

    created log out route and link

--- controllers/authController.js
+++ controllers/authController.js
@@ -62,4 +62,16 @@ router.post('/login', async (req,res) => {
 	}
 })
 
+
+router.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if(err){
+      res.send(err);
+    } else {
+      res.redirect('/auth/login');
+    }
+  })
+})
+
+
 module.exports = router;
