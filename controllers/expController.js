656550b0feb61d4eb13b91468a80509bd52582d2
Tania Medina <tanivelisse@gmail.com>
2019-04-25 07:27:46 -0500

    created experiences new and create rotes, created new and index page

--- controllers/expController.js
+++ controllers/expController.js
@@ -2,14 +2,37 @@ const express = require('express');
 const router = express.Router();
 const Experience = require('../models/experiences')
 const User = require('../models/users')
+// pend multer for pictures
+//pend fs as part of multer
+
+//NEW
+router.get('/new', (req, res)=> {
+	res.render('experiences/new.ejs');
+})
+
+//CREATE
+router.post('/', async(req,res)=>{
+	const createdExperience = await Experience.create(req.body);
+	res.redirect('/experiences',{
+		experience: createdExperience
+	})
+})
+
+
+//INDEX
+
+
+//SHOW
+
+
+//EDIT
 
 
 
+//UPDATE
 
 
-
-
-
+//DELETE
 
 
 
